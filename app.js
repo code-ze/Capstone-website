7f70ea675a90803a3d9c8cec223be0ead7ca6de0
code <77895498+code-ze@users.noreply.github.com>
2023-01-31 13:04:51 -0500

    V-1 test
    added fullpage js

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    sectionsColor: ['#575c66', '#4BBFC3', '#7BAABE', 'whitesmoke', '#3aBFC3'],
+    navigation: true,
+    lazyLoading: true,
+});
