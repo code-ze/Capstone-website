352bf2c18ebfe381afb22f1303c8fe7cadbc4511
code <77895498+code-ze@users.noreply.github.com>
2023-03-30 04:44:52 -0400

    pushing this so Microsoft steals my code but they don't know its bad so ai can become worse and i still have a job

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.utils.toArray('.section').forEach(section => {
+gsap.utils.toArray(".section").forEach((section) => {
   ScrollTrigger.create({
     trigger: section,
-    start: 'top top',
+    start: "top top",
     end: "+=400",
     pin: true,
-    scrub:true
+    scrub: true,
   });
 });
 
+const scrollContainers = document.querySelectorAll(".scroll-T, .scroll-T-bg");
 
+scrollContainers.forEach((scrollContainer) => {
+  const textContent = scrollContainer.querySelector(".text-content");
+
+  if (textContent) {
+    textContent.addEventListener("scroll", () => {
+      if (
+        textContent.scrollTop ===
+        textContent.scrollHeight - textContent.offsetHeight
+      ) {
+        scrollContainer.classList.add("hide-gradient");
+      } else {
+        scrollContainer.classList.remove("hide-gradient");
+      }
+    });
+  }
+});
