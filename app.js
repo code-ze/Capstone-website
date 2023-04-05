e7f2b354a0a8fef01765738e00ffdbcc1581d0fa
code <77895498+code-ze@users.noreply.github.com>
2023-04-05 10:10:19 -0400

    Prototype

--- app.js
+++ app.js
@@ -10,13 +10,47 @@ gsap.utils.toArray(".section").forEach((section) => {
   });
 });
 
-const scrollContainers = document.querySelectorAll(".scroll-T, .scroll-T-bg");
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollTextElements = document.querySelectorAll(
+    ".scroll-T-bg, .scroll-T"
+  ); // Add '.scroll-T' to the query selector
 
-var scrollT = document.querySelector(".scroll-T");
-scrollT.addEventListener("scroll", function () {
-  if (scrollT.scrollHeight - scrollT.scrollTop === scrollT.clientHeight) {
-    scrollT.classList.add("mask-removed");
-  } else {
-    scrollT.classList.remove("mask-removed");
+  scrollTextElements.forEach(function (scrollText) {
+    scrollText.addEventListener("scroll", function () {
+      const maxScroll = scrollText.scrollHeight - scrollText.clientHeight;
+      const scrollThreshold = maxScroll - 5; // Add a small threshold to account for minor differences in scroll height
+
+      if (scrollText.scrollTop >= scrollThreshold) {
+        scrollText.classList.add("no-gradient");
+        console.log("Gradient removed:", scrollText);
+      } else {
+        scrollText.classList.remove("no-gradient");
+        console.log("Gradient applied:", scrollText);
+      }
+    });
+  });
+});
+
+const documentHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
+};
+window.addEventListener("resize", documentHeight);
+documentHeight();
+
+const videoBackground = document.getElementById("video-background");
+const content = document.getElementById("content");
+
+// Choose how many seconds before the end of the video you want the animation to start
+const secondsBeforeEnd = 5;
+
+videoBackground.addEventListener("timeupdate", () => {
+  const targetTime = videoBackground.duration - secondsBeforeEnd;
+
+  if (videoBackground.currentTime >= targetTime) {
+    content.style.display = "flex";
+    content.classList.add("fade-in");
+    // Remove the event listener to prevent triggering the animation again
+    videoBackground.removeEventListener("timeupdate", arguments.callee);
   }
 });
